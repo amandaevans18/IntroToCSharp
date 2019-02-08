f26f0414fe6c2760340d88d3afdec508a211eb2f
amandaevans18 <42452129+amandaevans18@users.noreply.github.com>
2019-02-07 17:52:59 -0800

    Start of asssesment application as well as reflections

---
+++ PracticalCustomEquatableStructs/practicalCES/practicalCES/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+
+namespace practicalCES
+{
+    public struct Polygon2D
+    {
+        public Vector2[] vertices;
+        public float vertexCount { get { return vertices.Length; } }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        IEquatable<Polygon2D> Equatable(Polygon2D targetPoly2D)
+        {
+            bool doneChecking = false;
+
+            while (doneChecking)
+            {
+
+
+            }
+        }
+        
+    }
+}
---
+++ Reflection/ExternalLib/MyExternalClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExternalLib
+{
+    public class MyExternalClass
+    {
+        public static void TestHelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
